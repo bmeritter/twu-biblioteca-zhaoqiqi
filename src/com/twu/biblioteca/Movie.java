851eff9f9fefb8d7176ea2c6ca79495a9899cf44
bmeritter <bme_ritter@foxmail.com>
2017-08-03 09:31:05 +0800

    add Movie.java

--- src/com/twu/biblioteca/Movie.java
+++ src/com/twu/biblioteca/Movie.java
@@ -1,7 +1,36 @@
 package com.twu.biblioteca;
 
-/**
- * Created by ritter on 17-8-3.
- */
 public class Movie {
+  private String movieName;
+  private String year;
+  private String director;
+  private int rating;
+
+  public Movie(String movieName, String year, String director, int rating) {
+    this.movieName = movieName;
+    this.year = year;
+    this.director = director;
+    this.rating = rating;
+  }
+
+  public String getMovieName() {
+    return movieName;
+  }
+
+  public String getYear() {
+    return year;
+  }
+
+  public String getDirector() {
+    return director;
+  }
+
+  public int getRating() {
+    return rating;
+  }
+
+  public String getMovieDetail() {
+    return String.format("movieName:%-4s year:%-4s director:%-4s rating:%d", movieName, year, director, rating);
+  }
 }
+
