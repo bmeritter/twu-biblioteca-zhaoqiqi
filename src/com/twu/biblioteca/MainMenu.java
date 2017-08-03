3f03682c2e658640721cf4958acadc3c21227ea3
bmeritter <bme_ritter@foxmail.com>
2017-08-03 09:34:48 +0800

    update MainMenu.java

--- src/com/twu/biblioteca/MainMenu.java
+++ src/com/twu/biblioteca/MainMenu.java
@@ -1,32 +1,69 @@
 package com.twu.biblioteca;
 
 import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.List;
 
-/**
- * Created by ritter on 17-7-31.
- */
 public class MainMenu {
-  public static void main(String[] args) {
-    PrintStream printStream = System.out;
-    Console console = new Console(printStream);
+  Library library;
+  PrintStream printStream;
+  Console console;
 
+  public MainMenu(Library library, PrintStream printStream,
+              Console console) {
+    this.library = library;
+    this.printStream = printStream;
+    this.console = console;
   }
 
-  private static Library library() {
-    List<Book> listOfBooks = new ArrayList<Book>();
-    Book book1 = new Book("The Dispossessed", "Ursula K. Le Guin", "2001");
-    Book book2 = new Book("Island", "Aldous Huxley", "1953");
-    listOfBooks.add(book1);
-    listOfBooks.add(book2);
+  public void displayMenu() {
+    printStream.println("------------------------");
+    printStream.println("Please select an option:");
+    printStream.println("1. List Books");
+    printStream.println("2. Checkout Book");
+    printStream.println("3. Return Book");
+    printStream.println("4. List Movies");
+    printStream.println("5. Checkout Movie");
+    printStream.println("6. Return Movie");
+    printStream.println("7. Display User Information");
+    printStream.println("Q. Quit");
+    printStream.println("-------------------------");
+  }
 
-    List<Book> listOfCheckedOutBooks = new ArrayList<Book>();
-    Book checkedOutBook1 = new Book("The Name of the Wind", "Patrick Rothfuss", "2006");
-    Book checkedOutBook2 = new Book("Wise Man's Fear", "Patrick Rothfuss", "2010");
-    listOfCheckedOutBooks.add(checkedOutBook1);
-    listOfCheckedOutBooks.add(checkedOutBook2);
+  public void selectOption(int index) {
+    console.printWelcomeMessage();
+    displayMenu();
+    String input = console.getUserInput();
 
-    return new Library(listOfBooks, System.out);
+    while (!input.equals("Q")) {
+      if (input.equals("1")) {
+        library.displayBooksWithBookNumber();
+
+      } else if (input.equals("2")) {
+        library.checkoutBook();
+
+      } else if (input.equals("3")) {
+        library.returnBook();
+
+      } else if (input.equals("4")) {
+        library.displayMoviesWithMovieNumber();
+
+      } else if (input.equals("5")) {
+        library.checkoutMovie();
+
+      } else if (input.equals("6")) {
+        library.returnMovie();
+
+      } else if (input.equals("7")) {
+        library.displayUserInformation(index);
+
+        printStream.println("Select a valid option!");
+
+      } else {
+        printStream.println("Select a valid option!");
+
+      }
+      displayMenu();
+      input = console.getUserInput();
+    }
+
   }
 }
